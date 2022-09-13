fe61aaa6e111623620310804bd20568b0ab8a199
enea <eneadushkuwork@hotmail.com>
2022-09-13 11:36:33 +0200

    test 4

--- src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootBuildingblocksApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootBuildingblocksApplication.class, args);
+		SpringApplication.run(SpringbootBuildingblocksApplication.class, args);//
 	}
 
 }
